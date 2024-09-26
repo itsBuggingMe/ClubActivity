4aabc4023fbac48d9ef8b9e0ff60906b4bebe55b
Paperclip <131926845+itsBuggingMe@users.noreply.github.com>
2024-09-26 10:53:04 -0400

    Update MyProgram.cs

--- GuiClient.Desktop/MyProgram.cs
+++ GuiClient.Desktop/MyProgram.cs
@@ -7,34 +7,6 @@ internal class MyProgram
 {
     public Choice Choose(Round[] rounds)
     {
-        if(rounds.Length == 0)
-        {
-            return Choice.Cooperate;
-        }
-
-        Round lastRound = rounds.Last();
-
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice; return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice; return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-
-
-        return lastRound.OpponentChoice;
-
-        return lastRound.OpponentChoice; return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-        return lastRound.OpponentChoice;
-
-        return lastRound.OpponentChoice;
+        return Choice.Cheat;
     }
 }
